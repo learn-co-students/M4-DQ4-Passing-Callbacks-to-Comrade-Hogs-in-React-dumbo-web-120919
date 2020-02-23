001150b3fd5bfe63a2dceca40264e15423a09ab4
Annie Liao <liaowow@gmail.com>
2020-02-23 12:42:28 -0500

    Done.

--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -24,12 +24,13 @@ export default class PigPen extends React.Component {
     const newState = {environment: 'docile'}
     this.setState(newState)
   }
-
+  
   alterEnvironment = (vibe) => {
     if (vibe === "inhospitable")
-      this.audio.play()
+    this.audio.play()
     const newState = {environment: vibe}
     this.setState(newState)
+    setTimeout(this.relax, 2000)
   }
 
   generateSheeple = () => {
@@ -43,7 +44,7 @@ export default class PigPen extends React.Component {
     return(
       <div id="pig-pen">
         {sheeple}
-        <GalaxySNote7 environment={null} alterEnvironment={null} />
+        <GalaxySNote7 environment={this.state.environment} alterEnvironment={this.alterEnvironment} />
       </div>
     )
   }
